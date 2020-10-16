3bc0544f0c24adb5ef4a98adf261460347fd86d9
Prof. Cambieri Oscar <oscar.cambieri@vallauri.edu>
2020-10-16 13:09:11 +0200

    Lessons from 18 to 26 of Udemy Angular Course.

--- my-app/src/app/server/server.component.ts
+++ my-app/src/app/server/server.component.ts
@@ -5,5 +5,19 @@ import { Component } from '@angular/core';
     templateUrl: './server.component.html'
 })
 export class ServerComponent {
+    serverId: number;
+    serverStatus: string;
 
+    constructor(){
+        this.serverId = this.getRandomInt(1, 100);
+    }
+
+    getServerStatus() {
+        this.serverStatus = this.getRandomInt(0, 1) == 0 ? 'offline' : 'online';
+        return this.serverStatus;
+    }
+
+    getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
 }
